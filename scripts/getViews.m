b3d0387cc49f353ffd0b9c8836f5576c5726d645
jingdao <chenjingdao@wustl.edu>
2016-04-01 15:37:05 -0400

    refined importWeight interface

--- scripts/getViews.m
+++ scripts/getViews.m
@@ -6,8 +6,7 @@ volume_size = 24;
 pad_size = 3;
 data_size = 30;
 
-viewpoint=1;
-angle_inc=360;
+angle_inc=360/12;
 obj_center = [0,0,-2]'; % object coordinate in camera system
 camera_center = [0,0,0]'; camera_direction = [0,0,1]';
 axis = cross(camera_direction, obj_center - camera_center);
@@ -30,30 +29,32 @@ for c = 1 : num_classes
 		filename = files(i).name;
 		l = length(filename);
 		if (l>=5 && strcmp(filename(length(filename)-3:length(filename)),'.off'))
-			fprintf(lb,'%d %s\n',c,classes{c});
-			off_data = off_loader([category_path '/' filename],(viewpoint-1)*angle_inc);
-			instance = polygon2voxel(off_data,[volume_size,volume_size,volume_size],'auto');
-			instance_data = zeros(data_size,data_size,data_size);
-			i1 = 1 + pad_size;
-			i2 = pad_size + volume_size;
-			instance_data(i1:i2,i1:i2,i1:i2) = instance;
-			fwrite(cv,instance_data,'int8');
+			for viewpoint = 1 : 360/angle_inc
+				fprintf(lb,'%d %s\n',c,classes{c});
+				off_data = off_loader([category_path '/' filename],(viewpoint-1)*angle_inc);
+				instance = polygon2voxel(off_data,[volume_size,volume_size,volume_size],'auto');
+				instance_data = zeros(data_size,data_size,data_size);
+				i1 = 1 + pad_size;
+				i2 = pad_size + volume_size;
+				instance_data(i1:i2,i1:i2,i1:i2) = instance;
+				fwrite(cv,instance_data,'int8');
 
-			[depth_new, K, crop] = off2im([category_path '/' filename], 1, (viewpoint - 1) * angle_inc * pi / 180, R_o, obj_center(1), obj_center(2), obj_center(3), [1;1;1], 0, 0);
-			depth{1} = depth_new; R{1} = R_o; trans{1} = trans_o; mult = 5;
-			gridDists = TSDF(depth, K, obj_center, R, trans, volume_size * mult, pad_size * mult, [], crop);
-			gridDists = cubicle2col(gridDists, mult);
-			surface_num = sum((gridDists < 1 & gridDists > -1),1);
-			out_num = sum((gridDists == 1),1);
-			in_num = sum((gridDists == -1),1);
-			sur_index = (surface_num > 0 & in_num > 0 & out_num > 0) | surface_num > mult^2;
-			out_index = (out_num >= in_num) & ~sur_index;
-			gridDists = ones([data_size, data_size, data_size]);
-			gridDists = -1 * gridDists;
-			gridDists(sur_index) = 1;
-			gridDists(out_index) = 0;
-			gridDists = permute(gridDists, [3,1,2]);
-			fwrite(pv,gridDists,'int8');
+				[depth_new, K, crop] = off2im([category_path '/' filename], 1, (viewpoint - 1) * angle_inc * pi / 180, R_o, obj_center(1), obj_center(2), obj_center(3), [1;1;1], 0, 0);
+				depth{1} = depth_new; R{1} = R_o; trans{1} = trans_o; mult = 5;
+				gridDists = TSDF(depth, K, obj_center, R, trans, volume_size * mult, pad_size * mult, [], crop);
+				gridDists = cubicle2col(gridDists, mult);
+				surface_num = sum((gridDists < 1 & gridDists > -1),1);
+				out_num = sum((gridDists == 1),1);
+				in_num = sum((gridDists == -1),1);
+				sur_index = (surface_num > 0 & in_num > 0 & out_num > 0) | surface_num > mult^2;
+				out_index = (out_num >= in_num) & ~sur_index;
+				gridDists = ones([data_size, data_size, data_size]);
+				gridDists = -1 * gridDists;
+				gridDists(sur_index) = 1;
+				gridDists(out_index) = 0;
+				gridDists = permute(gridDists, [3,1,2]);
+				fwrite(pv,gridDists,'int8');
+			end
 			fprintf('Processed %s\n',filename);
 		end
 	end
--- scripts/getWeights.m
+++ scripts/getWeights.m
@@ -1,23 +1,27 @@
 function [] = getWeights()
 	g10 = load('generative_10_class.mat');
 	fid = fopen('generative_10_class.txt','w');
-	saveModel(g10.model,true,fid)
+	saveModel(g10.model,fid);
 	fclose(fid);
 	g40 = load('generative_40_class.mat');
 	fid = fopen('generative_40_class.txt','w');
-	saveModel(g40.model,true,fid)
+	saveModel(g40.model,fid);
 	fclose(fid);
 	d10 = load('discriminative_10_class.mat');
 	fid = fopen('discriminative_10_class.txt','w');
-	saveModel(d10.model,false,fid)
+	saveModel(d10.model,fid);
 	fclose(fid);
 	d40 = load('discriminative_40_class.mat');
 	fid = fopen('discriminative_40_class.txt','w');
-	saveModel(d40.model,false,fid)
+	saveModel(d40.model,fid);
+	fclose(fid);
+	ft = load('finetuned_model.mat');
+	fid = fopen('finetuned_model.txt','w');
+	saveModel(ft.model,fid);
 	fclose(fid);
 end
 
-function [] = saveModel(model,isGenerative,fid)
+function [] = saveModel(model,fid)
 	fprintf(fid,'numLayer: %d\n',model.numLayer);
 	fprintf(fid,'classes: %d\n',model.classes);
 	fprintf(fid,'validation: %d\n',model.validation);
@@ -26,18 +30,18 @@ function [] = saveModel(model,isGenerative,fid)
 	fprintf(fid,'pad_size: %d\n',model.pad_size);
 	for i=1:model.numLayer
 		fprintf(fid,'layer{%d}\n',i);
-		saveLayer(model.layers{i},isGenerative,fid);
+		saveLayer(model.layers{i},fid);
 	end
 end
 
-function [] = saveLayer(layer,isGenerative,fid)
+function [] = saveLayer(layer,fid)
 	fprintf(fid,'type: %s\n',layer.type);
 	fprintf(fid,'layerSize: ');
 	for i=1:length(layer.layerSize)
 		fprintf(fid,'%d ',layer.layerSize(i));
 	end
 	fprintf(fid,'\n');
-	if (!strcmp(layer.type,'input'))
+	if (~strcmp(layer.type,'input'))
 		fprintf(fid,'actFun: %s\n',layer.actFun);
 		if (strcmp(layer.type,'convolution'))
 			fprintf(fid,'stride: %d\n',layer.stride);
@@ -47,11 +51,23 @@ function [] = saveLayer(layer,isGenerative,fid)
 			end
 			fprintf(fid,'\n');
 		end
-		fprintf(fid,'w\n');
-		saveWeights(layer.w,fid);
-		fprintf(fid,'c\n');
-		saveWeights(layer.c,fid);
-		if (isGenerative)
+		if (isfield(layer,'w'))
+			fprintf(fid,'w\n');
+			saveWeights(layer.w,fid);
+		end
+		if (isfield(layer,'uw'))
+			fprintf(fid,'uw\n');
+			saveWeights(layer.uw,fid);
+		end
+		if (isfield(layer,'dw'))
+			fprintf(fid,'dw\n');
+			saveWeights(layer.dw,fid);
+		end
+		if (isfield(layer,'c'))
+			fprintf(fid,'c\n');
+			saveWeights(layer.c,fid);
+		end
+		if (isfield(layer,'b'))
 			fprintf(fid,'b\n');
 			saveWeights(layer.b,fid);
 		end
